ad10f6c1c65a992a2dc406574967e2566a170e19
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-03 19:13:58 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,11 @@
 class Backer
-  def initialize
+  def initialize(name)
     @backed_projects = []
+    @name = name
   end
   attr_reader :backed_projects
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self)
+  end
 end
