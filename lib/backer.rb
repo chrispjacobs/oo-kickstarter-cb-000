92fbd8eb26d29e7a340966c869d56d2faf507265
Christopher Jacobs <chrisj.3222@gmail.com>
2019-04-03 19:32:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,9 +6,14 @@ class Backer
   attr_reader :backed_projects, :name
   def back_project(project)
     @backed_projects << project
-    project.add_backer_minimalist(self)
-  end
-  def back_project_minimalist(project)
-    @backed_projects << project
+    already_backed = false
+    project.backers.each do |backer|
+      if backer == self
+        already_backed = true
+      end
+      if already_backed == false
+        project.add_backer(self)
+      end
+    end
   end
 end
